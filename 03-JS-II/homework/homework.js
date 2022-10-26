ee0f5a9d2522f04f9b2d0fe9790de4822dbc8f0b
Antonella Marrone <antomarrone1996@gmail.com>
2022-10-26 18:55:52 -0300

    aklsjd

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -44,12 +44,18 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero === 5 || numero === 10){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  var Saludo = "Hola";
 }
 
 function esEntero(numero) {
